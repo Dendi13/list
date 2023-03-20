289b6e84a7f45d01c14dc406abc68afbc41579f8
Dendi13 <98680907+Dendi13@users.noreply.github.com>
2023-03-20 11:27:38 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -3,9 +3,39 @@ let addBtn = document.querySelector('#addBtn');
 let list = document.querySelector('#list');
 
 addBtn.addEventListener('click', (evt) => {
+
     let li = document.createElement('li');
-    li.className('los');
+    li.classList.add('item');
     list.append(li);
     li.append(taskInput.value);
-    console.log('faa')
+    taskInput.value = '';
+
+    let deletes = document.createElement('div');
+    deletes.classList.add('item__btns');
+    li.append(deletes);
+    deletes.style.textAlign = 'right';
+
+    let mus = document.createElement('i');
+    mus.classList.add('fa-square-check');
+    mus.classList.add('fa-regular');
+    deletes.append(mus);
+
+    mus.addEventListener('click', (evt) => {
+        li.classList.toggle('done');
+    })
+
+    let cor = document.createElement('i');
+    cor.classList.add('fa-trash-can');
+    cor.classList.add('fa-solid');
+    deletes.append(cor);
+
+    cor.addEventListener('click', (evt) => {
+        list.removeChild(li);
+    })
 })
+
+
+
+
+
+
